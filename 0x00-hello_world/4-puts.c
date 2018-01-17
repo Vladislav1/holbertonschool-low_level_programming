fee71a4236c07b5f730ad1bc94df4fbacbc9bb34
315 <315@holbertonschool.com>
2018-01-17 23:09:07 +0000

    program that outputs a statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/* program prints statment using put */
+#include <stdio.h>
+
+main(void)
+{
+  char statement[] = "\"Programming is like building a multilingual puzzle";
+  
+  puts(statement);
+
+  return 0;
+}

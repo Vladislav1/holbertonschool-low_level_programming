079837563e175da8720e5a852c808ea2736f23cd
315 <315@holbertonschool.com>
2018-01-18 23:34:42 +0000

    script that prints 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints 00 to 99
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0', b = '0'; a <= '9'; a++)
+	{
+		putchar(b);
+		putchar(a);
+		if (b != '9' && a == '9')
+		{
+			b++;
+			a = '0';
+		}
+
+		if (b != '9' || a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

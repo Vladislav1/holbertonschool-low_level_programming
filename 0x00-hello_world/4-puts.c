03e69585ecea4f5d44edf333642619a245be8cfc
315 <315@holbertonschool.com>
2018-01-18 00:45:58 +0000

    program prints statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 /* program prints statment using put */
 #include <stdio.h>
-
+/**
+ * main - prints statment
+ * Return: returns 0 when program complete
+ */
 int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";

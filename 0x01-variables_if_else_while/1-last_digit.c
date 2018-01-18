a228fbda3329fb4afc344c6b6fc311296d2e6193
315 <315@holbertonschool.com>
2018-01-18 21:34:00 +0000

    script that prints last digit and whether it is bigger then, less then 5

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - tells how large last value is
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last_digit = n % 10;
+
+	if (last_digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	if (last_digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d is %d", n, last_digit);
+		printf(" and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
